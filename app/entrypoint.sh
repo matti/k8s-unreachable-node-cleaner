2f0b75b986fae2949d777ff02c10f02c98db2173
Matti Paksula <matti.paksula@iki.fi>
2023-11-16 11:54:18 +0200

    :sunrise:

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-echo "starting"
+_on_error() {
+  trap '' ERR
+  line_path=$(caller)
+  line=${line_path% *}
+  path=${line_path#* }
+
+  echo ""
+  echo "ERR $path:$line $BASH_COMMAND exited with $1"
+  _shutdown 1
+}
+trap '_on_error $?' ERR
+
+_shutdown() {
+  trap '' TERM INT
+  echo "shutdown: $1"
+
+  kill 0
+  wait
+  exit "$1"
+}
+trap '_shutdown 0' TERM INT
 
 delay=${1:-5}
+
+echo "starting with delay: $delay"
+
+until kubectl get nodes ; do
+  echo "failed to get nodes, retrying in $delay"
+  sleep "$delay"
+done
 
 nodes_last_unreachable=""
 while true; do
@@ -15,7 +42,7 @@ while true; do
     for node_last_unreachable in $nodes_last_unreachable; do
       if [ "$node_now_unreachable" == "$node_last_unreachable" ]; then
         echo "$(date) still unreachable $node_now_unreachable, deleting"
-        kubectl delete node "$node_now_unreachable"
+        kubectl delete node "$node_now_unreachable" || echo "failed to delete"
       fi
     done
   done
